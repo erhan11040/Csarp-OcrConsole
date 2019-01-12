1e5bafc1af0a70c4c0d0629fbe7c271c3937b1bf
HeathCliff <erhan11040@gmail.com>
2019-01-12 21:40:34 +0300

    Project Uploaded

---
+++ OcrConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tesseract;
+namespace OcrConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("asdasd");
+            var image = new Bitmap(@"Z:\NewProject\demo\image.bmp");
+            TesseractEngine ocr;
+            
+
+        }
+    }
+}
